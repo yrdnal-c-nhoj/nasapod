75a42282810d80a19d26dd2fa0c9e4e20d1a18c6
john-c-landry <johnclandry@yahoo.com>
2026-03-12 18:07:30 -0400

    x

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,5 @@ app.get('/api/apod', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5009;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
